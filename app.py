bfdd36c8f164ce0b7eab80b3c4b70f82a166d1e9
Your Name <853211b@gmail.com>
2016-06-20 00:22:51 +0300

    a

--- app.py
+++ app.py
@@ -11,90 +11,64 @@ import datetime
 import os
 
 app=Flask(__name__)
-def pushHer():
-	os.chdir("gitPush")
-	call("git init",shell=True)
-
-	call("ssh-keygen -f ~/.ssh/id_rsa -t rsa -N '' -y",shell=True)
-	ssh=open("../.ssh/id_rsa.pub")
-	ssh=ssh.readline().rstrip()
-	call('curl -u "taketa:weuwdfyu7" --data \'{"title":"test-key","key":"%s"}\' https://api.github.com/user/keys' % ssh,shell=True)
-
-	call("git remote add origin git@github.com:taketa/iptv.git",shell=True)
-	call("git add .",shell=True)
-
-	call('git config --global user.email "you@example.com" && git config --global user.name "Your Name"',shell=True) 
-
-
-	call("git pull origin master",shell=True)
-	call('git commit -am "ok"',shell=True)
-
-
-
-	call("git push origin master",shell=True)
-	return "ok"
-def run(url):
-	p = Popen(["timeout","20s","ffprobe",  url], stdout=PIPE, stderr=PIPE)
-	stdout, stderr = p.communicate()
-	test=re.findall(',\s.*kb/s',stderr)
-	if test: return "good"
-	else: return "bad"
-
-def getm3(url="http://iptv.slynet.tv/FreeSlyNet.m3u"):
-    m3=open("gitPush/forTest.m3u")
-    # m3=urllib2.urlopen(url)
-    m3=m3.read()    
-    out=m3.split("\n#EXTINF:-1,")
-    out=[i.split("\n") for i in out if "PremiumSlyNet" not in i]
-    out=[i for i in out if len(i)==2]
-    out=[i for i in out if "===" not in i[0]]
-    good={}  
-    for i in out:
-        test=run(i[1].split(" ")[0])
-        print(test)
-        print(i[1].split(" ")[0])
-        if test=="good":
-            good[i[0]]=i[1]
-    
-	f=open('nStream.xml','w')
-	f.write('<?xml version="1.0" encoding="UTF-8" ?>\n<items>\n<playlist_name>IPTV</playlist_name>\n<all_region></all_region>\n')
-	f.close()
-	f=open('gitPush/nStream.xml','a')
-	for i in good:
-	    f.write("<channel>\n<title><![CDATA[%s]]></title>\n<logo_30x30><![CDATA[]]></logo_30x30>\n<description></description>\n<stream_url><![CDATA[%s]]></stream_url>\n</channel>\n" % (i,good[i])) 
-	f.close()
-
-
-	f=open('gitPush/iptv.m3u', 'w')
-	f.write("#EXTM3U\n")
-	f.close()
-	for i in good:
-	    f=open('gitPush/iptv.m3u', 'a')
-	    f.write("#EXTINF:-1,"+i+"\n"+good[i]+"\n")
-	    f.close()
-	
-    return "ok"
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
-
-# шифрование
-
-    return jsonify(result="Caesar said that the text is not encrypted")
 
 @app.route('/')
 def index():
-    getm3()
-    pushHer()
+	def pushHer():
+		os.chdir("gitPush")
+		call("git add .",shell=True)
+		call('git config --global user.email "853211b@gmail.com" && git config --global user.name "Your Name"',shell=True) 
+		call("git pull origin master",shell=True)
+		call('git commit -am "ok"',shell=True)
+		call("git push origin master",shell=True)
+		return "ok"
+	def run(url):
+		p = Popen(["timeout","20s","ffprobe",  url], stdout=PIPE, stderr=PIPE)
+		stdout, stderr = p.communicate()
+		test=re.findall(',\s.*kb/s',stderr)
+		if test: return "good"
+		else: return "bad"
+
+	def getm3(url="http://iptv.slynet.tv/FreeSlyNet.m3u"):
+	    m3=open("gitPush/forTest.m3u")
+	    # m3=urllib2.urlopen(url)
+	    m3=m3.read()    
+	    out=m3.split("\n#EXTINF:-1,")
+	    out=[i.split("\n") for i in out if "PremiumSlyNet" not in i]
+	    out=[i for i in out if len(i)==2]
+	    out=[i for i in out if "===" not in i[0]]
+	    good={}  
+	    for i in out:
+	        test=run(i[1].split(" ")[0])
+	        print(test)
+	        print(i[1].split(" ")[0])
+	        if test=="good":
+	            good[i[0]]=i[1]
+	    
+		f=open('gitPush/nStream.xml','w')
+		f.write('<?xml version="1.0" encoding="UTF-8" ?>\n<items>\n<playlist_name>IPTV</playlist_name>\n<all_region></all_region>\n')
+		f.close()
+		f=open('gitPush/nStream.xml','a')
+		for i in good:
+			f.write("<channel>\n<title><![CDATA[%s]]></title>\n<logo_30x30><![CDATA[]]></logo_30x30>\n<description></description>\n<stream_url><![CDATA[%s]]></stream_url>\n</channel>\n" % (i,good[i])) 
+		f.close()
+
+
+		f=open('gitPush/iptv.m3u', 'w')
+		f.write("#EXTM3U\n")
+		f.close()
+		f=open('gitPush/iptv.m3u', 'a')
+		for i in good:
+		    
+		    f.write("#EXTINF:-1,"+i+"\n"+good[i]+"\n")
+		f.close()
+		
+	    return "ok"
+	getm3()
+	pushHer()
 
    
-    return "ok"
+	return "ok"
     # return send_file('iptv.m3u')
     
 

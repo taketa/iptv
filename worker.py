eada34125c646700bb076419f3781fa6ff239991
Your Name <853211b@gmail.com>
2016-06-20 15:07:29 +0300

    a

--- worker.py
+++ worker.py
@@ -12,7 +12,7 @@ import json
 def push():
 	f=open('gitPush/iptv.m3u')
 	
-	r = requests.post("/data", data=f)
+	r = requests.post("data", data=f)
 	f.close()
 def run(url):
 	p = Popen(["timeout","20s","ffprobe",  url], stdout=PIPE, stderr=PIPE)

d4d2e1b711a25a247e1c6a5e76191207b204ba5c
Your Name <853211b@gmail.com>
2016-06-20 15:30:35 +0300

    a

--- app.py
+++ app.py
@@ -5,21 +5,16 @@ import json
 from flask import send_file
 
 app=Flask(__name__)
-@app.route('/')
-def index():
-
-	pid = Popen(["python worker.py"], shell=True,stdin=None, stdout=None, stderr=None, close_fds=True)  
-	return "ok"
-@app.route("/dataWork",methods = ['POST'])
+@app.route("/",methods = ['POST'])
 def dat():
 	if request.method == 'POST':
 		
-		f=open("gitPush/iptv.m3u","w")
+		f=open("iptv.m3u","w")
 		f.write(request.data)
 		f.close()
 	return "ok"
 @app.route("/dataGet")
 def dataGet():
-	return send_file("gitPush/iptv.m3u")
+	return send_file("iptv.m3u")
 if __name__=="__main__":
 	app.run()

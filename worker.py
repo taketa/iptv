a9a35b94710a345e84df290358302be697582c84
Your Name <853211b@gmail.com>
2016-06-20 12:41:07 +0300

    a

--- worker.py
+++ worker.py
@@ -32,7 +32,7 @@ def pushHer():
 	call("git init",shell=True)
 	call("git remote add origin git@github.com:taketa/iptv.git",shell=True)
 	call("git add .",shell=True)
-	call('git config --global user.email "853211b@gmail.com" && git config --global user.name "Your Name"',shell=True) 
+	call('git config --global user.email "853211b@gmail.com" && git config --global user.name "taketa"',shell=True) 
 	call("git pull origin master",shell=True)
 	call('git commit -am "ok"',shell=True)
 	call("git pull origin master",shell=True)

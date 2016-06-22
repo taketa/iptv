94ff35f1416ec7f45006171ee3e94e672efd7a08
Your Name <you@example.com>
2016-06-22 13:40:24 +0300

    a

--- app.py
+++ app.py
@@ -6,26 +6,11 @@ import json
 from flask import send_file
 
 app=Flask(__name__)
-<<<<<<< HEAD
-@app.route("/",methods = ['GET','POST'])
-def dat():
-	if request.method == 'POST':
-		
-		f=open("iptv.m3u","w")
-		f.write(request.data)
-		f.close()
-	
-	return "ok"
-@app.route("/get")
-def dataGet():
-	return send_file("iptv.m3u")
-=======
 @app.route('/')
 def index():
 
 	pid = Popen(["python worker.py"], shell=True,stdin=None, stdout=None, stderr=None, close_fds=True)  
 	return "ok"
 
->>>>>>> 43c9413c3308b0083729e89dd2b2bb01c88fad5e
 if __name__=="__main__":
 	app.run()
